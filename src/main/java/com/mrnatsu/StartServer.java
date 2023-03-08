f45cf83ded64a7a8f5c725d51130f30bbd9b2806
NgDinhKhiem <87563740+NgDinhKhiem@users.noreply.github.com>
2023-03-08 12:08:41 +0700

    upload

--- src/main/java/com/mrnatsu/StartServer.java
+++ src/main/java/com/mrnatsu/StartServer.java
@@ -1,5 +1,41 @@
 package com.mrnatsu;
 
-public class StartServer {
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.GameMode;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerBlockBreakEvent;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
 
+public class StartServer {
+    public static void main(String[] args){
+        MinecraftServer minecraftServer = MinecraftServer.init();
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        // Create the instance
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        // Set the ChunkGenerator
+        instanceContainer.setGenerator(unit ->
+                unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+        globalEventHandler.addListener(PlayerBlockBreakEvent.class,event ->{
+            final Player player = event.getPlayer();
+            player.setGameMode(GameMode.CREATIVE);
+            player.getInventory().addItemStack(ItemStack.of(event.getResultBlock().));
+        });
+        // Start the server on port 25565
+        minecraftServer.start("0.0.0.0", 25565);
+    }
 }
